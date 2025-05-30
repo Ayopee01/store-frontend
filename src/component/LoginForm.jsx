e301457cc811bd68798081904fea2e9d12ab00ee
Ayopee01 <Ayopee001@gmail.com>
2025-05-30 18:30:32 +0700

    fix axios

--- src/component/LoginForm.jsx
+++ src/component/LoginForm.jsx
@@ -28,7 +28,7 @@ function LoginForm() {
     setLoading(true);
 
     try {
-      const res = await axios.post(`${import.meta.env.VITE_API_URL}/auth/login`, form); // ✅ แก้ตรงนี้
+      const res = await axios.post(`${API_URL}/auth/login`, form); // ✅ แก้ตรงนี้
 
       if (res.data.success) {
         localStorage.setItem("user", JSON.stringify(res.data.user));
